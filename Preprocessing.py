10fbb612d4a7fca2606d9952818f0cd26912863b
MAX <slemuh.ms@gmail.com>
2019-04-15 22:48:52 -0500

    Added all weights and notebooks, presentation is coming

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,13 +1,14 @@
-import nltk
+# import nltk
 from string import punctuation
 import pickle
-from nltk.tokenize import TweetTokenizer,word_tokenize
+# from nltk.tokenize import TweetTokenizer,word_tokenize
 # nltk.download("stopwords")
-from nltk.corpus import stopwords
-from nltk.stem import SnowballStemmer
-import itertools
+# from nltk.corpus import stopwords
+# from nltk.stem import SnowballStemmer
+import numpy as np
+# import itertools
 import emoji
-
+import math
 # #" & ".join(["%.4f"%(sum([i[n] for i in a])/5) for n in range(4)])
 
 # import ptvsd
@@ -18,10 +19,10 @@ import emoji
 # ptvsd.wait_for_attach()
 
 # ejemplo de uso
-# python3 Preprocessing.py 1 Data/ Experimento2/
+# python3 Preprocessing.py 1 Data/ Experimento2/ 300 0 5
 
 
-def preprocessing(corpus,getVocabulary):
+def preprocessing(corpus,getVocabulary,lenPad,vocabulario=None):
     twits=[]
     with open(corpus,"r") as f:
         docs=f.read().splitlines()
@@ -30,105 +31,126 @@ def preprocessing(corpus,getVocabulary):
     # letras=[("á","a"),("é","e"),("í","i"),("ó","o"),("ú","u"),("ü","u")]
     cosas=[("\\n","\n"),("http://link","η"),("\\\"","\"")]
     twitsVectors=[]
-    vocabularioBueno=["UNK"]
+    vocabularioBueno=["NTG","UNK"]
     dcont=1/len(twits)
     for t in twits:
         print("Llevo {0}".format(cont),end="\r")
         # t[1]=t[1].lower()
+        
         tnGrams=[]
         for c,v in cosas:
             t[1]=t[1].replace(c,v)
         # tnGrams=t[1]
+        original=t[1]
+        vector=[] 
+        t[1]="ŧ"+t[1]+"¶"
         for i in range(len(t[1])-(n-1)):
             tnGrams.append(t[1][i:i+n])        
             if getVocabulary:
                 if tnGrams[-1] not in vocabularioBueno:
                     vocabularioBueno.append(tnGrams[-1])##Aqui va lo de los vectores
+                vector.append(vocabularioBueno.index(tnGrams[-1]))
+            else:
+                if tnGrams[-1] not in vocabulario:
+                    vector.append(vocabulario.index("UNK"))
+                else:
+                    vector.append(vocabulario.index(tnGrams[-1]))
+
         # input(tnGrams)
+        # if getVocabulary:
+        #     vector.append(vocabularioBueno.index("¶")) #simbolo de fin de línea
+        #     # falta=math.ceil((lenPad-len(vector))/2)
+        #     # vector=[vocabularioBueno.index("NTG")]*falta+vector+[vocabularioBueno.index("NTG")]*falta
+        #     # vector=vector[:lenPad]
+        # else:
+        #     vector.append(vocabulario.index("¶")) #simbolo de fin de línea
+            # falta=math.ceil((lenPad-len(vector))/2)
+            # vector=[vocabulario.index("NTG")]*falta+vector+[vocabulario.index("NTG")]*falta
+            # vector=vector[:lenPad]
+        # vector= np.array((vector*(math.ceil(lenPad/len(vector))))[:lenPad],dtype=np.float32)
         
-        twitsVectors.append([int(t[0]), list(tnGrams)])
+        # input(vector)
+        if(len(vector)==0):
+            print(vector)
+            print(original)
+            input(t[1])
+        twitsVectors.append((float(t[0]), vector))
         # input(twitsVectors[-1])
         cont+=dcont
 
     if getVocabulary:
-        return twitsVectors,vocabularioBueno
+        return np.array(twitsVectors,dtype=object),vocabularioBueno
     else:
-        return twitsVectors
+        return np.array(twitsVectors,dtype=object)
 
-def SplitHash(hashTag):
-    salida=[""]
-    contM=0
-    contm=0
-    for c in hashTag:
-        if c.isupper():
-            if salida[-1]!="" :
-                salida.append(c)
-            else:
-                salida[-1]+=c
-            contM+=1
-        else:
-            salida[-1]+=c
-            contm+=1
-    if contM==len(hashTag) or contm==len(hashTag) or sum([1 for i in salida if len(i)==1])>len(salida)*0.5:
-        return [hashTag]
-    else:
-        return salida
+# def SplitHash(hashTag):
+#     salida=[""]
+#     contM=0
+#     contm=0
+#     for c in hashTag:
+#         if c.isupper():
+#             if salida[-1]!="" :
+#                 salida.append(c)
+#             else:
+#                 salida[-1]+=c
+#             contM+=1
+#         else:
+#             salida[-1]+=c
+#             contm+=1
+#     if contM==len(hashTag) or contm==len(hashTag) or sum([1 for i in salida if len(i)==1])>len(salida)*0.5:
+#         return [hashTag]
+#     else:
+#         return salida
 
-def wordVect2Vect(twits,vocabulario):
-    vectores=[]
-    cont=0
-    dcont=1/len(twits)
-    for t in twits:
-        vAux=[]
-        print("Llevo ",cont,end="\r")
-        # input(t)
-        for p in t[1]:
-            if p in vocabulario:
-                vAux.append(vocabulario.index(p))
-            else:
-                vAux.append(vocabulario.index("UNK"))
-        vectores.append([t[0],vAux])
-        # input(vectores[-1])
-        cont+=dcont
-    return vectores    
+# def wordVect2Vect(twits,vocabulario,lenPad):
+#     vectores=[]
+#     cont=0
+#     dcont=1/len(twits)
+#     for t in twits:
+#         vAux=[]
+#         print("Llevo ",cont,end="\r")
+#         # input(t)
+#         for p in t[1]:
+#             if p in vocabulario:
+#                 vAux.append(vocabulario.index(p))
+#             else:
+#                 vAux.append(vocabulario.index("UNK"))
+        
+#         vAux=(vAux*(math.ceil(lenPad/len(vAux))))[:lenPad]
+        
+#         vectores.append([t[0],vAux])
+#         # input(vectores[-1])
+#         cont+=dcont
+#     return vectores    
 
-def getVectors(archivoEntr,archivoPrue,archivoSalpickle,vocabularioF):
+def getVectors(archivoEntr,archivoPrue,archivoSalpickle,vocabularioF,lenPad):
     print("Inicia el preprocesamiento de {0} y {1}".format(archivoEntr,archivoPrue))
-    twitsTrain,vocabulario=preprocessing(archivoEntr,True)
+    twitsTrain,vocabulario=preprocessing(archivoEntr,True,lenPad)
     pickle.dump(vocabulario,open(vocabularioF,"wb"))
     print("Termine de encontrar el vocab y preprocessar el entrenamiento")
-    twitsTest=preprocessing(archivoPrue,False)
-    print("Termine de preprocesar la prueba")
-    vectoresTrain=(wordVect2Vect(twitsTrain,vocabulario))
-    print("Termine de encontrar los vectores de entrenamiento")
-    vectoresTest=(wordVect2Vect(twitsTest,vocabulario))
-    print("Termine de encontrar los vectores de prueba")
-
-    print("Guardando")
-
-    vTrx=[]
-    vTry=[]
-    vTsx=[]
-    vTsy=[]
-    for v in vectoresTrain:
-        vTry.append(v[0])
-        vTrx.append(v[1])
-    for v in vectoresTest:
-        vTsy.append(v[0])
-        vTsx.append(v[1])
-    pickle.dump([(vTrx,vTry),(vTsx,vTsy)],open(archivoSalpickle,"wb"))
+    twitsTest=preprocessing(archivoPrue,False,lenPad,vocabulario)    
+    print("\nGuardando\n")
+    pickle.dump([(twitsTrain[:,1],twitsTrain[:,0]),(twitsTest[:,1],twitsTest[:,0])],open(archivoSalpickle,"wb"))
 import sys 
 
 n=int(sys.argv[1])
 baseData=sys.argv[2]
 base=sys.argv[3]
+lenPad=int(sys.argv[4])
+rango1=int(sys.argv[5])
+rango2=int(sys.argv[6])
 lista=[[baseData+"corpusTrain%i.txt"%i,
 baseData+"corpusTest%i.txt"%(i),
 base+"vectoresSet%i_g%i.pkl"%(i,n),
-base+"vocabulario%i_g%i.pkl"%(i,n)] for i in range(5)
+base+"vocabulario%i_g%i.pkl"%(i,n)] for i in range(rango1,rango2)
     ]
-
+print("Procesaré:\n"+"\n".join([" ".join(l) for l in lista]))
+aceptar=input("Aceptar(Y/n):")
+if(aceptar=="Y" or aceptar=="y" or aceptar==""):
+    pass
+else:
+    exit()
 for docs in lista:
     print("Preprocesando {0},{1},{2},{3}".format(docs[0],docs[1],docs[2],docs[3]))
-    getVectors(docs[0],docs[1],docs[2],docs[3])
+    getVectors(docs[0],docs[1],docs[2],docs[3],lenPad)
 print("Termine todo.")
--- PreprocessingWord.py
+++ PreprocessingWord.py
@@ -1,38 +1,50 @@
-from sklearn.metrics import accuracy_score,confusion_matrix,recall_score,precision_score,f1_score
+from sklearn.metrics import accuracy_score, confusion_matrix, recall_score, precision_score, f1_score
 from unicodedata import normalize
 import numpy as np
 import re
 import sys
 import pickle
-
+import math
 # ejemplo de uso
-# python3 PreprocessingWord.py Data/ Experimento1/
+# python3 PreprocessingWord.py Data/ Experimento1/ 100
+
+# import ptvsd
+# # Allow other computers to attach to ptvsd at this IP address and port.
+# ptvsd.enable_attach(address=('127.0.0.1', 5677), redirect_output=True)
+
+# # Pause the program until a remote debugger is attached
+# ptvsd.wait_for_attach()
+
 
 def noCaracteresEspeciales(texto):
 	# -> NFD y eliminar diacríticos
 	texto = re.sub(
-        r"([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+", r"\1", 
-        normalize( "NFD", texto), 0, re.I
+        r"([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+", r"\1",
+        normalize("NFD", texto), 0, re.I
     )
 
 	# -> NFC
-	return re.sub('\W',' ',texto).lower()
+	return re.sub('\W', ' ', texto).lower()
 
-def crearVocabulario(archCorpus,umbral):
+
+def crearVocabulario(archCorpus, umbral):
 	"""Con esta función creamos el vocabulario a partir del archCorpus
-	INPUT: archCorpus- nombre del archivo con el corpus, 
+	INPUT: archCorpus- nombre del archivo con el corpus,
 		umbral- numero de apariciones minima para considerarlas relevantes
 	OUTPUT: el vocabulario"""
-	corpus=open(archCorpus,"r").readlines()
-	vocabulario={"UNK":0}
+	corpus = open(archCorpus, "r").readlines()
+	vocabulario = {"UNK":0}
+	cosas = [("\\n", "\n"), ("http://link", "η"), ("\\\"", "\"")]
 	for twit in corpus:
 		twitSplit=noCaracteresEspeciales(twit)
+		for c, v in cosas:
+			twitSplit=twitSplit.replace(c,v)
+		twitSplit="ŧ"+twitSplit+"¶"
 		for palabra in twitSplit.split():
-			if palabra!="http://link":
-				if palabra not in vocabulario:
-					vocabulario[palabra]=1
-				else:
-					vocabulario[palabra]+=1
+			if palabra not in vocabulario:
+				vocabulario[palabra]=1
+			else:
+				vocabulario[palabra]+=1
 	ignorados=0
 	for palabra in list(vocabulario):
 		if vocabulario[palabra]<umbral:
@@ -40,29 +52,31 @@ def crearVocabulario(archCorpus,umbral):
 			vocabulario.pop(palabra)
 			ignorados+=1
 	print("ignore ",ignorados," palabras")
+	
 	return vocabulario
 
-def convertirAVector(oracion,dic):
+def convertirAVector(oracion,dic,lenPad):
 	vector=[]
 	texto=noCaracteresEspeciales(oracion).split()
+	texto = ["ŧ"]+texto+["¶"]
 	for t in texto:
 		try:
 			vector.append(dic.index(t))
 		except:
 			vector.append(dic.index("UNK"))
+	vector=(vector*(math.ceil(lenPad/len(vector))))[:lenPad]
 	return vector
 
 
-def corpusAVectores(corpuses,vocabulario,separacion):
+def corpusAVectores(corpuses,dic,separacion,lenPad):
 	# try:
-	dic=list(vocabulario.keys())
 	# print(corpuses)
 	with open(corpuses[0],"r") as f:
 		c=f.readlines()
-		vect_train=np.array(list(map(lambda x:(int(x[0]),convertirAVector(x[1],dic)),[i.split(separacion,1) for i in c ])),dtype=object)
+		vect_train=np.array(list(map(lambda x:(float(x[0]),convertirAVector(x[1],dic,lenPad)),[i.split(separacion,1) for i in c ])),dtype=object)
 	with open(corpuses[1],"r") as f:
 		c=f.readlines()
-		vect_test=np.array(list(map(lambda x:(int(x[0]),convertirAVector(x[1],dic)),[i.split(separacion,1) for i in c ])),dtype=object)
+		vect_test=np.array(list(map(lambda x:(float(x[0]),convertirAVector(x[1],dic,lenPad)),[i.split(separacion,1) for i in c ])),dtype=object)
 		# print(vect_test)
 	return [(vect_train[:,1],vect_train[:,0]),(vect_test[:,1],vect_test[:,0])]
 	# pickle.dump([(vect_train[:,1],vect_train[:,0]),(vect_test[:,1],vect_test[:,0]),dic],open(archivoPickle,"wb"))
@@ -72,18 +86,19 @@ def corpusAVectores(corpuses,vocabulario,separacion):
 	# 	print("Ha ocurrido un error: ",e)
 	# 	return 0
 
-def getVectors(archivoEntr,archivoPrue,archivoSalpickle,vocabularioF):
-	vocabulario=crearVocabulario(
+def getVectors(archivoEntr,archivoPrue,archivoSalpickle,vocabularioF,lenPad):
+	vocabulario=list(crearVocabulario(
 		archivoEntr #el corpus de entrenamiento
 		,5 #umbral a partir de cuantas apariciones se considera relevante
-		)
+		))
 	pickle.dump(vocabulario,open(vocabularioF,"wb"))
 	print("Inicia el preprocesamiento de {0} y {1}".format(archivoEntr,archivoPrue))
-	vectores=corpusAVectores([archivoEntr,archivoPrue],vocabulario,"|")
+	vectores=corpusAVectores([archivoEntr,archivoPrue],vocabulario,"|",lenPad)
 	pickle.dump(vectores,open(archivoSalpickle,"wb"))
 	print("Guardando")
 baseData=sys.argv[1]
 base=sys.argv[2]
+lenPad=int(sys.argv[3])
 lista=[[baseData+"corpusTrain%i.txt"%i,
 baseData+"corpusTest%i.txt"%(i),
 base+"vectoresSet%i_palabras.pkl"%(i),
@@ -91,5 +106,5 @@ base+"vocabulario%i_palabras.pkl"%(i)] for i in range(5)
 ]
 for docs in lista:
     print("Preprocesando {0},{1},{2}".format(docs[0],docs[1],docs[2],docs[3]))
-    getVectors(docs[0],docs[1],docs[2],docs[3])
+    getVectors(docs[0],docs[1],docs[2],docs[3],lenPad)
 print("Termine todo.")
